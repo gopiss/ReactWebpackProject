bed87088828f931cbd677207ac9bd11e562925dd
‘Denzil’Dsouza’ <denzildsouza3661@gmail.com>
2023-03-20 19:38:58 +0530

    added h3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>
+        <h1 className="heading1">Hello, Welcome to my website</h1>
+        <h6 className="heading6">where coals turn into dimonds</h6>
+      </div>
+      <h3>hiii</h3>
+      <div>
+        <img
+          className="fill"
+          src="https://static-koimoi.akamaized.net/wp-content/new-galleries/2022/12/robert-downey-jr-is-back-baby-iron-man-is-finally-coming-back-to-avengers-001.jpg"
+          alt="Lam"
+        />
+      </div>
+    </>
+  );
 };
 
 export default App;
